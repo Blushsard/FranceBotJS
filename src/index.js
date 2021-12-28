b24a7f20e9bf1e3d58d649fcf59e88d760a2364d
snowb <snow.benjamin2@gmail.com>
2021-12-28 17:28:00 +0100

    Reworked completely the functions loading the commands and events in the client and the function loading the command in the guild and guilds. Removed the now useless registerCommands.js file as the new functions are much better. Updated index.js with the new function. Update of the packages files because one new package (glob) is needed to load the commands and events in the client.

--- src/index.js
+++ src/index.js
@@ -5,10 +5,9 @@
  */
 
 
-const { TOKEN, GUILD_ID } = require( "./files/config.json" );
+const { TOKEN, DEV_GUILD_ID } = require( "./files/config.json" );
 const { Client, Collection, Intents } = require( "discord.js" );
 const { loadCommands, loadEvents } = require( "./utils/loadAssets" );
-const { loadCommandsToGuild } = require( "./utils/registerCommands" );
 
 
 const client = new Client({
@@ -30,8 +29,10 @@ client.commands = new Collection();
 	await loadCommands( client );
 	await loadEvents( client );
 	await client.login( TOKEN );
+
 	// Out-comment the following line if the commands where modified/added to upload the commands to the guild.
-	// await loadCommandsToGuild( client.user.id, GUILD_ID, TOKEN );
+	// const { loadCommandsToGuild } = require( "./utils/loadAssets" );
+	// await loadCommandsToGuild( client, DEV_GUILD_ID );
 })();
 
 
