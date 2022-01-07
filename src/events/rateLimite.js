7857a3c13a5ce6faf739c1f51910a6ddb7d7657e
LeMecChelou <snow.benjamin2@gmail.com>
2022-01-07 21:23:39 +0100

    Ajout du de l'évènement rateLimit.

---
+++ src/events/rateLimite.js
@@ -0,0 +1,22 @@
+/**
+ * @author Benjamin Guirlet
+ * @description
+ *      Cet évènement permet d'indiquer quand le bot atteint la limite de requêtes par seconde avec l'api discord.
+ */
+
+
+/* ----------------------------------------------- */
+/* FUNCTIONS                                       */
+/* ----------------------------------------------- */
+async function execute( rateLimit ) {
+	console.log( `\033[31mIMPORTANT\033[0m : ${rateLimit}` );
+}
+
+
+/* ----------------------------------------------- */
+/* MODULE EXPORTS                                  */
+/* ----------------------------------------------- */
+module.exports = {
+	name: "rateLimit",
+	execute
+}
