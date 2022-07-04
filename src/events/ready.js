a5b7a1a0f7fd08cde53c1f0f59ef482866d9d146
uAtomicBoolean <benjamin.guirlet@gmail.com>
2022-07-04 16:37:37 +0200

    Changement de l'affichage de la date du rate limite (update de la timezone sur le vps). Ajout de la date lors de la connexion du bot et envoi d'un mp pour signaler la connexion.

--- src/events/ready.js
+++ src/events/ready.js
@@ -5,14 +5,28 @@
  *		permisssions, etc.
  */
 
+const { Client, MessageEmbed } = require( "discord.js" );
+
 
 /* ----------------------------------------------- */
 /* FUNCTIONS                                       */
 /* ----------------------------------------------- */
+/**
+ * Event appelé lorsque le bot est connecté.
+ * @param {Client} client Le client du bot.
+ */
 async function execute( client ) {
-	console.log( `${client.user.username} is connected!` );
+	const date = new Date();
+	console.log( `${client.user.username} is connected at ${date}!` );
 	await client.modules.get( "moyenne" ).calcMoyenne( 60_000 );
 	await client.modules.get( "feed" ).feed( 60_000);
+
+	// Envoi du message de connexion en pm.
+	const recipient = await client.users.fetch( process.env.RECIPIENT_CONN_MSG );
+	const connEmbed = new MessageEmbed()
+		.setTitle( "Bot connecté!" )
+		.setDescription( date.toString() );
+	await recipient.send( { embeds: [ connEmbed ] } );
 }
 
 
