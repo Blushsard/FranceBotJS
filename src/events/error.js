5f3813cf3a53a677a4b7e74bf577b5b90c334307
uAtomicBoolean <benjamin.guirlet@gmail.com>
2022-07-08 15:05:40 +0200

    Correction de bugs, ajout de la commande moyenne et update de textes dans les commandes.

---
+++ src/events/error.js
@@ -0,0 +1,25 @@
+/**
+ * @author Benjamin Guirlet
+ * @description
+ *      Permet d'afficher les erreurs de l'API discord.
+ */
+
+/* ----------------------------------------------- */
+/* FUNCTIONS                                       */
+/* ----------------------------------------------- */
+/**
+ * Event appelé lorsque le bot est connecté.
+ * @param {Error} error L'erreur envoyée par l'API.
+ */
+async function execute( error ) {
+	console.log( error );
+}
+
+
+/* ----------------------------------------------- */
+/* MODULE EXPORTS                                  */
+/* ----------------------------------------------- */
+module.exports = {
+	name: "error",
+	execute
+}
