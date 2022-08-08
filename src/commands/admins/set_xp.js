2b4765385de688bb5d87ecfe221dc3f11b0253f0
uAtomicBoolean <benjamin.guirlet@gmail.com>
2022-08-09 01:50:37 +0200

    Ajout de la commande set_xp et correction d'un bug bloquant l'ajout de l'xp dans les salons n'étant pas dans la bdd.

---
+++ src/commands/admins/set_xp.js
@@ -0,0 +1,84 @@
+/**
+ * @author Benjamin Guirlet
+ * @description
+ *      Permet de définir l'xp d'un utilisateur à la main.
+ */
+const { SlashCommandBuilder } = require( "@discordjs/builders" );
+const { CommandInteraction } = require( "discord.js" );
+
+
+/* ----------------------------------------------- */
+/* COMMAND BUILD                                   */
+/* ----------------------------------------------- */
+const slashCommand = new SlashCommandBuilder()
+	.setName( "set_xp" )
+	.setDescription( "Permet de définir le nombre d'xp d'un utilisateur à la main." )
+	.addUserOption( option =>
+		option
+			.setName( "user" )
+			.setDescription( "L'utilisateur qui recevra la modification d'xp." )
+			.setRequired( true )
+	)
+	.addNumberOption( option =>
+		option
+			.setName( "xp" )
+			.setDescription( "Le nombre d'xp à ajouté." )
+			.setRequired( true )
+	)
+	.setDefaultPermission(false);
+
+
+/* ----------------------------------------------- */
+/* FUNCTIONS                                       */
+/* ----------------------------------------------- */
+/**
+ * Handler pour la SlashCommande.
+ * @param {CommandInteraction} interaction L'interaction générée par l'exécution de la commande.
+ */
+async function execute( interaction ) {
+	const usersManager = interaction.client.db.usersManager;
+	const levels = interaction.client.modules.get( "levels" );
+	const finalXp = interaction.options.getNumber( "xp" );
+
+	const member = await interaction.guild.members.fetch( interaction.options.getUser( "user" ).id );
+	let userDb = await usersManager.fetchUser( member.id );
+
+	if ( !userDb ) {
+		await usersManager.addUser(
+			member.id,
+			levels.getLevelFromXp( finalXp ),
+			finalXp,
+			0,
+			0
+		);
+	}
+	else {
+		await usersManager.updateUser(
+			interaction.options.getUser( "user" ).id,
+			"n_xp",
+			finalXp
+		);
+	}
+
+	userDb = await usersManager.fetchUser( member.id );
+	await levels.refreshUser( member, userDb );
+
+	try {
+		await interaction.reply({
+			content: `L'xp de ${member} a été mise à ${finalXp}!`,
+			ephemeral: true
+		});
+	}
+	catch ( err ) {
+		console.log( "Interaction inconnue: commands/admin/set_xp.js:51" );
+	}
+}
+
+
+/* ----------------------------------------------- */
+/* MODULE EXPORTS                                  */
+/* ----------------------------------------------- */
+module.exports = {
+	data: slashCommand,
+	execute
+}
--- src/modules/Levels.js
+++ src/modules/Levels.js
@@ -40,8 +40,7 @@ class Levels
 	 * @param {object|null} channel Le salon de la base de données dans lequel le message a été envoyé.
 	 */
 	async ajouterExperienceMessage( message, channel ) {
-		if ( !channel) return;
-		if ( channel['b_exp'] ) return;
+		if ( channel && channel['b_exp'] ) return;
 		if ( !this._active || message.author.bot ) return;
 		if ( message.channel instanceof DMChannel ) return;	// On empêche les gens de gagner de l'xp avec les DM du bot.
 
