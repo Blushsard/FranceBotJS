b0a8d3d6b4ab1275e1721e0c2e6372b759b525d3
AtomicBoolean <benjamin.guirlet@gmail.com>
2022-08-21 00:12:43 +0200

    Préparation des fonctions pour les logs.

--- src/modules/Logs.js
+++ src/modules/Logs.js
@@ -21,6 +21,39 @@ class Logs
 
 	set active( active ) { this._active = active; }
 	get active() { return this._active; }
+
+	/**
+	 * Log envoyé quand un message est envoyé dans un salon de memes (likes).
+	 */
+	async messageMemeEnvoye() {}
+
+	/**
+	 * Log envoyé quand un message dans un salon de memes (likes) est supprimé.
+	 */
+	async memeSupprime() {}
+
+	/**
+	 * Log envoyé quand un message est supprimé par repost.
+	 * Ce log est envoyé avant le log de suppression de message. Les informations de ce log sont donc moindre qu'un
+	 * log de suppression de message car le log suivant contiendra les informations nécessaires.
+	 */
+	async repostSupprime() {}
+
+	/**
+	 * Log envoyé quand un message est envoyé dans le feed ou sur reddit ou sur twitter.
+	 */
+	async memeEnvoye() {}
+
+	/**
+	 * Log envoyé quand la boucle de reddit ou twitter fait une itération.
+	 * Le feed n'est pas danc ce log car il tourne toutes les secondes.
+	 */
+	async iteractionModule() {}
+
+	/**
+	 * Log envoyé quand un like ou un repost est ajouté/enlevé d'un message contenant des memes.
+	 */
+	async modificationVote() {}
 }
 
 
