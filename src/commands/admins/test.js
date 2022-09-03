c0dd92ca31b46b9dd0719ae571c80d6e76c7f26f
uAtomicBoolean <benjamin.guirlet@gmail.com>
2022-09-03 16:57:14 +0200

    Ajout d'une commande test.

--- src/commands/admins/test.js
+++ src/commands/admins/test.js
@@ -5,7 +5,8 @@ const { SlashCommandBuilder } = require( "@discordjs/builders" );
 /* COMMAND BUILD                                   */
 /* ----------------------------------------------- */
 const slashCommand = new SlashCommandBuilder()
-	.setName( "ping" );
+	.setName( "ping" )
+	.setDescription( "pong" );
 
 async function execute( interaction ) {
 	await interaction.reply( "Pong !" );
